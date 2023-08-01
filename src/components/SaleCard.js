fa5a88dfedc61e97da5da79712c00cdbc6aa58cc
icedev214 <icecold3214@gmail.com>
2023-08-01 08:29:28 +0200

    Fix sale card style

--- src/components/SaleCard.js
+++ src/components/SaleCard.js
@@ -10,15 +10,24 @@ import {
 
 const SaleCard = ({ data }) => {
   return (
-    <Card elevation={2} sx={{ mx: 0.4, height: "100%", borderRadius: 0 }}>
-      <CardContent sx={{ px: 2.5, py: 2.5 }}>
+    <Card
+      elevation={2}
+      sx={{
+        mx: 0.4,
+        height: "100%",
+        borderRadius: 0,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <CardContent sx={{ px: 2.5, py: 2.5, flexGrow: 1 }}>
         <Typography sx={{ fontSize: 20 }} gutterBottom>
           {data.name}
         </Typography>
         <Typography sx={{ fontSize: 14 }}>{data.description}</Typography>
       </CardContent>
       <Divider />
-      <CardActions sx={{ px: 2.5, py: 1.5, flexShrink: 0 }}>
+      <CardActions sx={{ px: 2.5, py: 1.5 }}>
         <Typography>{`£${data.price}`}</Typography>
         <Box sx={{ flexGrow: 1 }} />
         <Button variant="contained" color="green" sx={{ color: "white" }}>
