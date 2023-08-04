8bd9194b6d6c5b153eeeae990f61bee7209119bc
icedev214 <icecold3214@gmail.com>
2023-08-04 19:19:21 +0200

    Implement open restaurant status menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,56 @@
-import { AppBar, Toolbar, Button, Box, useMediaQuery } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  useMediaQuery,
+  Popper,
+  Paper,
+  Typography,
+  Divider,
+} from "@mui/material";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import SearchField from "components/SearchField";
 
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 const NavBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
   const navigate = useNavigate();
 
   const { page } = useSelector((state) => state.navigation);
 
+  const weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
   const down_lg_matches = useMediaQuery((theme) =>
     theme.breakpoints.down("lg")
   );
+
+  const handlePopperOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopperClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -46,25 +78,83 @@ const NavBar = () => {
               <Box sx={{ flexGrow: 1 }} />
             </>
           ) : null}
-          <Button
-            variant="standard"
-            startIcon={<AccessTimeIcon sx={{ width: 30, height: 30 }} />}
-            sx={{
-              color: "white",
-              fontSize: 14,
-              borderRadius: 0,
-              height: 64,
-              minWidth: 24,
-              "& .MuiButton-startIcon": {
-                marginLeft: "0px",
-                marginRight: down_lg_matches ? "0px" : "6px",
-              },
-              px: 1.6,
-            }}
-            disableRipple
-          >
-            {down_lg_matches ? "" : "Open"}
-          </Button>
+          <Box onMouseEnter={handlePopperOpen} onMouseLeave={handlePopperClose}>
+            <Button
+              variant="text"
+              startIcon={<AccessTimeIcon sx={{ width: 30, height: 30 }} />}
+              sx={{
+                color: "white",
+                fontSize: 14,
+                borderRadius: 0,
+                height: 64,
+                minWidth: 24,
+                "& .MuiButton-startIcon": {
+                  marginLeft: "0px",
+                  marginRight: down_lg_matches ? "0px" : "6px",
+                },
+                px: 1.6,
+                cursor: "default",
+              }}
+              style={{
+                backgroundColor: anchorEl ? "#0000001a" : "transparent",
+              }}
+              disableRipple
+            >
+              {down_lg_matches ? "" : "Open"}
+            </Button>
+            <Popper
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              placement="bottom-end"
+              sx={{ zIndex: (theme) => theme.zIndex.drawer + 2 }}
+            >
+              <Paper
+                sx={{
+                  borderRadius: 0,
+                  boxShadow: 3,
+                  px: 3,
+                  py: 4,
+                }}
+              >
+                <Typography sx={{ fontSize: 24, color: "black" }} gutterBottom>
+                  We are open now!
+                </Typography>
+                <Divider />
+                <Typography
+                  sx={{
+                    fontSize: 17,
+                    fontWeight: 600,
+                    color: "green.main",
+                    my: 2.5,
+                  }}
+                >
+                  Opening hours
+                </Typography>
+                {weekdays.map((day, index) => {
+                  return (
+                    <Box key={index}>
+                      <Box sx={{ display: "flex" }}>
+                        <Typography
+                          sx={{
+                            color: "black",
+                            fontWeight: 600,
+                            fontSize: 15,
+                          }}
+                        >{`${day}:`}</Typography>
+                        <Box sx={{ flexGrow: 1 }} />
+                        <Typography sx={{ color: "black", fontSize: 15 }}>
+                          0:00 - 0:00
+                        </Typography>
+                      </Box>
+                      {index == weekdays.length - 1 ? null : (
+                        <Divider sx={{ my: 1.5 }} />
+                      )}
+                    </Box>
+                  );
+                })}
+              </Paper>
+            </Popper>
+          </Box>
           <Button
             variant="standard"
             startIcon={<ExitToAppIcon sx={{ width: 30, height: 30 }} />}
@@ -77,6 +167,9 @@ const NavBar = () => {
               "& .MuiButton-startIcon": {
                 marginLeft: "0px",
                 marginRight: down_lg_matches ? "0px" : "6px",
+              },
+              "&:hover": {
+                backgroundColor: "#0000001a",
               },
               px: 1.6,
             }}
@@ -100,6 +193,9 @@ const NavBar = () => {
                 marginLeft: "0px",
                 marginRight: down_lg_matches ? "0px" : "6px",
               },
+              "&:hover": {
+                backgroundColor: "#0000001a",
+              },
               px: 1.6,
             }}
             disableRipple
@@ -109,6 +205,17 @@ const NavBar = () => {
           >
             {down_lg_matches ? "" : "Sign Up"}
           </Button>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+              height: 64,
+            }}
+          >
+            <ShoppingCartIcon sx={{ width: 30, height: 30 }} />
+          </Box>
         </Toolbar>
       </AppBar>
       <Offset />
